d87a024faed65f8a5f2cbb2cb98164ef6e8793c8
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-05-16 21:58:45 +0530

    Navbar and Layout skeleton ready

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { theme } from "@/chakra/theme";
+import Layout from "@/components/NextLayout/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
