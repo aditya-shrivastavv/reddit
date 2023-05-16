53b939bc85964b943c082e1a0ef7262edc88c507
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-05-16 22:33:43 +0530

    navbar idea

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,22 @@
+import { Flex, Image } from "@chakra-ui/react";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Flex bgColor={"white"} h={"44px"} p={"6px 12px"}>
+        <Flex align={"center"}>
+          <Image src="/images/redditFace.svg" alt="" h={"30px"} />
+          <Image
+            src="/images/redditText.svg"
+            alt=""
+            h={"46px"}
+            display={{ base: "none", md: "unset" }}
+          />
+        </Flex>
+        {/* <Directory /> */}
+        {/* <SearchBar /> */}
+        {/* <RightContent /> */}
+      </Flex>
+    </div>
+  );
 }
