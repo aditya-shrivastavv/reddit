b12478a8b527058cdf86e272a786b9fc8f3b79e3
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-05-17 11:02:54 +0530

    log in sign up buttons styled

--- src/components/Navbar/NavMenu/LogSignButtons.tsx
+++ src/components/Navbar/NavMenu/LogSignButtons.tsx
@@ -3,8 +3,25 @@ import { Button } from "@chakra-ui/react";
 export default function LogSignButtons() {
   return (
     <>
-      <Button>Log In</Button>
-      <Button>Sign Up</Button>
+      <Button
+        variant={"outline"}
+        h={"28px"}
+        display={{ base: "none", sm: "flex" }}
+        w={{ base: "70px", md: "110px" }}
+        mr={2}
+        onClick={() => console.log("Log In")}
+      >
+        Log In
+      </Button>
+      <Button
+        h={"28px"}
+        display={{ base: "none", sm: "flex" }}
+        w={{ base: "70px", md: "110px" }}
+        mr={2}
+        onClick={() => console.log("Sign Up")}
+      >
+        Sign Up
+      </Button>
     </>
   );
 }
