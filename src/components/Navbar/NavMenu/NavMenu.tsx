22293a36623ccbead9a4216225d5f55f86c219cf
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-05-21 11:40:00 +0530

    user can signOut

--- src/components/Navbar/NavMenu/NavMenu.tsx
+++ src/components/Navbar/NavMenu/NavMenu.tsx
@@ -1,13 +1,22 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import LogSignButtons from "./LogSignButtons";
 import AuthModal from "@/components/Modal/Auth/AuthModal";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebaseInit";
+import { signOut } from "firebase/auth";
 
 export default function NavMenu() {
+  const [user] = useAuthState(auth);
+
   return (
     <>
       <AuthModal />
       <Flex justify={"center"} align={"center"}>
-        <LogSignButtons />
+        {user ? (
+          <Button onClick={() => signOut(auth)}>Logout</Button>
+        ) : (
+          <LogSignButtons />
+        )}
       </Flex>
     </>
   );
