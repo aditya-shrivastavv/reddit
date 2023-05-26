f5c17bb9271eb4738b496e8fcc9e7fa4f9c63ad6
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-05-26 22:36:17 +0530

    responsiveness tackled

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,12 +2,25 @@ import { Flex, Image } from "@chakra-ui/react";
 import SearchBar from "./SearchBar";
 import NavMenu from "./NavMenu/NavMenu";
 import CommunityDropMenu from "./CommunityDropMenu/CommunityDropMenu";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebaseInit";
 
 export default function Navbar() {
+  const [user] = useAuthState(auth);
+
   return (
     <div>
-      <Flex bgColor={"white"} h={"44px"} p={"6px 12px"}>
-        <Flex align={"center"}>
+      <Flex
+        bgColor={"white"}
+        h={"44px"}
+        p={"6px 12px"}
+        justify={{ md: "space-between" }}
+      >
+        <Flex
+          align={"center"}
+          w={{ base: "40px", md: "auto" }}
+          mr={{ base: 0, md: 2 }}
+        >
           <Image src="/images/redditFace.svg" alt="" h={"30px"} />
           <Image
             src="/images/redditText.svg"
@@ -16,7 +29,7 @@ export default function Navbar() {
             display={{ base: "none", md: "unset" }}
           />
         </Flex>
-        <CommunityDropMenu />
+        {user && <CommunityDropMenu />}
         <SearchBar />
         <NavMenu />
       </Flex>
--- src/components/Navbar/SearchBar.tsx
+++ src/components/Navbar/SearchBar.tsx
@@ -1,9 +1,13 @@
+import { auth } from "@/firebase/firebaseInit";
 import { SearchIcon } from "@chakra-ui/icons";
 import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function SearchBar() {
+  const [user] = useAuthState(auth);
+
   return (
-    <Flex flexGrow={1} mr={2} align={"center"}>
+    <Flex flexGrow={1} mr={2} align={"center"} maxW={user ? "auto" : "600px"}>
       <InputGroup>
         <InputLeftElement pointerEvents={"none"}>
           <SearchIcon color={"gray.400"} mb={1} />
